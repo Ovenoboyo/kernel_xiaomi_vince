82ca84a9c711691b09d65e07abca1c92e526c2a8
Oveno <ovenoboyo@gmail.com>
2018-10-29 12:57:14 +0530

    Build script: Cleanups, add sudo, make test zips seperately
    Signed-off-by: Oveno <ovenoboyo@gmail.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # Copyright (C) 2018 Luan Halaiko (tecnotailsplays@gmail.com)
 #                    Abubakar Yagob (abubakaryagob@gmail.com)
+#                    Sahil Gupte (Ovenoboyo@gmail.com)
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -17,6 +18,18 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#git sudo
+if [ "$UID" != "0" ]; then
+   if whereis sudo &>/dev/null; then
+     echo "Please type the sudo password for the user $USER"
+     sudo $PWD/build.sh
+
+   else
+     echo "Sudo not found. You will need to run this script as root."
+     exit
+   fi 
+fi
+
 #Colors
 black='\033[0;30m'
 red='\033[0;31m'
@@ -27,48 +40,59 @@ purple='\033[1;35m'
 cyan='\033[0;36m'
 nc='\033[0m'
 
+
 #Directories
 KERNEL_DIR=$PWD
-KERN_IMG=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out/arch/arm64/boot/Image.gz-dtb
-ZIP_DIR=/media/oveno/649AC4299AC3F6181/zucc/Output/Zipper
+TOOL_DIR=$KERNEL_DIR/../toolchains
+PRODUCT_DIR=$KERNEL_DIR/../Output
+KERN_IMG=$PRODUCT_DIR/build/out/arch/arm64/boot/Image.gz
+DTB_T=$PRODUCT_DIR/build/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-vince-t.dtb
+DTB=$PRODUCT_DIR/build/out/arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-vince-nt.dtb
+ZIP_DIR=$PRODUCT_DIR/Zipper
 CONFIG_DIR=$KERNEL_DIR/arch/arm64/configs
+LOG_DIR=$PRODUCT_DIR/log
+OUT_DIR=$PRODUCT_DIR/build/out
+
+#Setup directories
+if [ ! -d "$PRODUCT_DIR" ]; then
+  mkdir $PRODUCT_DIR
+  mkdir $PRODUCT_DIR/build
+  mkdir $PRODUCT_DIR/prev
+  git clone https://github.com/Ovenoboyo/zucc_zipper.git $PRODUCT_DIR/Zipper
+fi
+
+#Setup toolchains
+if [ ! -d "$TOOL_DIR" ]; then
+  mkdir $TOOL_DIR
+  git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 ../toolchains/aarch64-linux-android-4.9
+  git clone git://github.com/krasCGQ/aarch64-linux-android -b opt-gnu-8.x --depth=1 ../toolchains/aarch64-linux-android-8.x
+fi   
+
+cd $KERNEL_DIR
 
 #Export
-export CROSS_COMPILE=/media/oveno/649AC4299AC3F6181/xtended/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android- 
+export CROSS_COMPILE=$TOOL_DIR/aarch64-linux-android-8.x/bin/aarch64-opt-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_USER="Ovenoboyo"
-export KBUILD_BUILD_HOST="Kekboi"
-
-#clang
-#export CLANG_COMPILE=true
-#export CLANG_TRIPLE=aarch64-linux-android-
-#export CLANG_PATH="/media/oveno/649AC4299AC3F6181/xtended/prebuilts/clang/host/linux-x86/clang-4053586"
-#CC="/media/oveno/649AC4299AC3F6181/xtended/prebuilts/clang/host/linux-x86/clang-4053586/bin/clang"
+export KBUILD_BUILD_USER="Goodboyo"
+export KBUILD_BUILD_HOST="loss"
 
 #Misc
 CONFIG=zucc_defconfig
 THREAD="-j4"
 
-#Main script
-while true; do
-echo -e "\n$green[1] Build Kernel"
-echo -e "[2] Regenerate defconfig"
-echo -e "[3] Source cleanup"
-echo -e "[4] Create flashable zip"
-echo -e "$red[5] Quit$nc"
-echo -ne "\n$brown(i) Please enter a choice[1-6]:$nc "
-
-read choice
-
-if [ "$choice" == "1" ]; then
+#begin functions
+makekornel()
+{
   BUILD_START=$(date +"%s")
   DATE=`date`
-  echo -e "\n$cyan#######################################################################$nc"
   echo -e "$brown(i) Build started at $DATE$nc"
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out $CONFIG $THREAD &>/dev/null \
-						     
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out $THREAD &>/media/oveno/649AC4299AC3F6181/zucc/Output/log/Buildlog.txt & pid=$! \
+  rm -rf $LOG_DIR
+  rm -rf $KERN_IMG
+  mkdir $LOG_DIR
+  git log -n 100 > $LOG_DIR/Changelog.txt
+  make O=$OUT_DIR $CONFIG $THREAD &>/dev/null \						     
+  make O=$OUT_DIR $THREAD &>$LOG_DIR/Buildlog.txt & pid=$! \
 
   spin[0]="$blue-"
   spin[1]="\\"
@@ -87,7 +111,8 @@ if [ "$choice" == "1" ]; then
   if ! [ -a $KERN_IMG ]; then
     echo -e "\n$red(!) Kernel compilation failed, See buildlog to fix errors $nc"
     echo -e "$red#######################################################################$nc"
-    exit 1
+    #Cause i m lazy af and geenome iz goooooddd
+    gedit $LOG_DIR/Buildlog.txt
   fi
   $DTBTOOL -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/ &>/dev/null &>/dev/null
 
@@ -97,82 +122,86 @@ if [ "$choice" == "1" ]; then
   echo -e "$cyan#######################################################################$nc"
   echo -e "$purple(i) Total time elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nc"
   echo -e "$cyan#######################################################################$nc"
+}
+
+regen_def()
+{
+  make O=$OUT_DIR  $CONFIG
+  cp $OUT_DIR/.config $CONFIG_DIR/$CONFIG
+  echo -e "$purple(i) Defconfig generated.$nc"
+}
+
+clean_sauce()
+{
+  rm -rf $OUT_DIR
+  echo -e "$purple(i) Kernel source cleaned up.$nc"
+}
+
+make_zip()
+{
+  cd $ZIP_DIR
+  make clean &>/dev/null
+  cp $LOG_DIR/Changelog.txt $ZIP_DIR/Changelog.txt
+  cp $KERN_IMG $ZIP_DIR/kernel/Image.gz
+  cp $DTB $ZIP_DIR/non-treble/
+  cp $DTB_T $ZIP_DIR/treble/
+  make &>/dev/null
+  cd $KERNEL_DIR
+  echo -e "$purple(i) Flashable zip generated under $ZIP_DIR.$nc"
+}
+
+make_zip_test()
+{
+  cd $ZIP_DIR
+  make clean &>/dev/null
+  cp $LOG_DIR/Changelog.txt $ZIP_DIR/Changelog.txt
+  cp $KERN_IMG $ZIP_DIR/kernel/Image.gz
+  cp $DTB $ZIP_DIR/non-treble/
+  cp $DTB_T $ZIP_DIR/treble/
+  make test &>/dev/null
+  cd $KERNEL_DIR
+  echo -e "$purple(i) Flashable zip (TEST) generated under $ZIP_DIR.$nc"
+}
+
+#Main script
+while true; do
+echo -e "\n$green[1] Build Kernel"
+echo -e "[2] Regenerate defconfig"
+echo -e "$red[3] Source cleanup"
+echo -e "$green[4] Create flashable zip"
+echo -e "$green[5] Create flashable zip (test build)"
+echo -ne "\n$brown(i) Please enter a choice[1-5]:$nc "
+
+read choice
+
+if [ "$choice" == "1" ]; then
+  echo -e "\n$cyan#######################################################################$nc"
+  makekornel
 fi
 
 if [ "$choice" == "2" ]; then
   echo -e "\n$cyan#######################################################################$nc"
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out  $CONFIG
-  cp /media/oveno/649AC4299AC3F6181/zucc/Output/build/out/.config arch/arm64/configs/$CONFIG
-  echo -e "$purple(i) Defconfig generated.$nc"
+  regen_def
   echo -e "$cyan#######################################################################$nc"
 fi
 
 if [ "$choice" == "3" ]; then
   echo -e "\n$cyan#######################################################################$nc"
-  rm -f $DT_IMG
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out clean &>/dev/null
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out mrproper &>/dev/null
-  rm -rf /media/oveno/649AC4299AC3F6181/zucc/Output/build/out
-  echo -e "$purple(i) Kernel source cleaned up.$nc"
+  clean_sauce
   echo -e "$cyan#######################################################################$nc"
 fi
 
 
 if [ "$choice" == "4" ]; then
   echo -e "\n$cyan#######################################################################$nc"
-  cd $ZIP_DIR
-  make clean &>/dev/null
-  cp $KERN_IMG $ZIP_DIR/Image.gz-dtb
-  make &>/dev/null
-  make sign &>/dev/null
-  cd /media/oveno/649AC4299AC3F6181/zucc/bak/EAS
-  echo -e "$purple(i) Flashable zip generated under $ZIP_DIR.$nc"
+  make_zip
   echo -e "$cyan#######################################################################$nc"
 fi
 
 if [ "$choice" == "5" ]; then
- exit 1
-fi
-
-if [ "$choice" == "6" ]; then
-  BUILD_START=$(date +"%s")
-  DATE=`date`
   echo -e "\n$cyan#######################################################################$nc"
-  echo -e "$brown(i) Build started at $DATE$nc"
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out $CONFIG $THREAD &>/dev/null \
-						     
-  make O=/media/oveno/649AC4299AC3F6181/zucc/Output/build/out  CONFIG_DEBUG_SECTION_MISMATCH=y $THREAD &>/media/oveno/649AC4299AC3F6181/zucc/Output/log/Buildlog.txt & pid=$! \
-
-  spin[0]="$blue-"
-  spin[1]="\\"
-  spin[2]="|"
-  spin[3]="/$nc"
-
-  echo -ne "$blue[Please wait...] ${spin[0]}$nc"
-  while kill -0 $pid &>/dev/null
-  do
-    for i in "${spin[@]}"
-    do
-          echo -ne "\b$i"
-          sleep 0.1
-    done
-  done
-  if ! [ -a $KERN_IMG ]; then
-    echo -e "\n$red(!) Kernel compilation failed, See buildlog to fix errors $nc"
-    echo -e "$red#######################################################################$nc"
-    exit 1
-  fi
-  $DTBTOOL -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/ &>/dev/null &>/dev/null
-
-  BUILD_END=$(date +"%s")
-  DIFF=$(($BUILD_END - $BUILD_START))
-  echo -e "\n$brown(i)Image-dtb compiled successfully.$nc"
-  echo -e "$cyan#######################################################################$nc"
-  echo -e "$purple(i) Total time elapsed: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nc"
+  make_zip_test
   echo -e "$cyan#######################################################################$nc"
 fi
+
 done
-
-
-make CONFIG_DEBUG_SECTION_MISMATCH=y
-
